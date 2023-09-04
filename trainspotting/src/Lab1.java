1495e9b5bda98f9839d309796c0b7a12a7f8b23a
Noah <noahsu@chalmers.se>
2023-09-04 14:49:28 +0200

    Test push

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -7,6 +7,10 @@ public class Lab1 {
 
     try {
       tsi.setSpeed(1,speed1);
+      tsi.setSpeed(2,speed2);
+      tsi.setSwitch(17, 7,0x02);
+      tsi.setSwitch(15, 9,0x02);
+      tsi.setSwitch(3, 11,0x02);
     }
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
