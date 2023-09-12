1a8d8a63731dedef212a737c83f52a451fb7faad
Noah <noahsu@chalmers.se>
2023-09-12 21:50:14 +0200

    Sensorer P-R har implementerats

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -1,32 +1,53 @@
 import TSim.*;
 import java.util.concurrent.Semaphore;
+import java.lang.Math;
+
 
 public class Lab1 {
 
   public Lab1(int speed1, int speed2) {
     TSimInterface tsi = TSimInterface.getInstance();
-    Train t1 = new Train(tsi,speed1,0,1);
-    Train t2 = new Train(tsi,speed2,1,2);
-    while(true){
-      t1.run();
-      t2.run();
+    //Train t1 = new Train(tsi,speed1,true,1);
+    //Train t2 = new Train(tsi,speed2,false,2);
+    Thread t1 = new Thread(new Train(tsi,speed1,true,1));
+    Thread t2 = new Thread(new Train(tsi,speed2,false,2));
+
+    try {
+      //tsi.setSpeed(2, speed2);
+      tsi.setSwitch(15, 9, 0x02);
+      tsi.setSwitch(4, 9, 0x01);
+      tsi.setSwitch(17, 7, 0x02);
+      
+    } catch (Exception e) {
+      // TODO: handle exception
     }
+    t1.start();
+    t2.start();
     
+
   }
 
   class Train implements Runnable{
   TSimInterface tsi;
   int speed;
-  int id;
-  int dir;
-  Semaphore sem;
+  int id; //1 för övre tåget, 2 för det undre
+  boolean dir; //TRUE om tåg kör nedåt, FALSE om de kör uppåt
+  Semaphore stn1_over = new Semaphore(0);
+  Semaphore stn1_under = new Semaphore(1);
+  Semaphore stn2_over = new Semaphore(0);
+  Semaphore stn2_under = new Semaphore(1);
+  Semaphore curve_right = new Semaphore(1);
+  Semaphore curve_left = new Semaphore(1);
+  Semaphore straight1_over = new Semaphore(1);
+  Semaphore straight1_under = new Semaphore(1);
+  Semaphore straight2_over = new Semaphore(1);
+  Semaphore straight2_under = new Semaphore(1);
 
-    Train(TSimInterface tsi, int speed, int dir, int id){
+    Train(TSimInterface tsi, int speed, boolean dir, int id){
       this.tsi = tsi;
       this.speed = speed;
       this.id = id;
       this.dir = dir;
-      this.sem = sem;
     }
 
     public void run(){
@@ -38,38 +59,75 @@ public class Lab1 {
         int xPos = sens.getXpos();
         int yPos = sens.getYpos();
         int stat = sens.getStatus();
-        if(this.speed > 0 & this.id == 1){
-          dir = 1;
-        }
-        else if(this.speed > 0 & this.id == 2){
-          dir = 0;
-        }
-        //kommer från vänster
-        else if(this.speed < 0 & this.id == 1){
-          dir = 0;
-        }
-        else if(this.speed < 0 & this.id == 2){
-          dir = 1;
-        }
-        //kommer från höger
         try {
-          sem.acquire();
+          while(true){ //längst upp
+            //tågstation 1, övre spåret
+            if(xPos==6 & yPos==6 & stat==0x01){
+              if(dir==true){
+                //Thread.sleep(1000 + (20 * Math.abs(speed)));
+                //tsi.setSpeed(id,0);                
+              }
+              else{
+                stn1_over.release();
+              }
 
+            }
+            //switch 4
+            //Q
+            if(xPos==5 & yPos==11 & stat==0x01){
+              if(dir==false){
+                curve_left.acquire();
+                tsi.setSwitch(3,11,0x01);
+                stn2_over.release();
+              }
+              else{
+                Thread.sleep(1000 + (20 * Math.abs(speed)));
+                tsi.setSpeed(id,0);
+                Thread.sleep(1000 + (20 * Math.abs(speed)));
+                speed = - this.speed;
+                tsi.setSpeed(id,speed);
+              }
+            }
+            //R
+            if(xPos==3 & yPos==11 & stat==0x01){
+              if(dir==false){
+                curve_left.acquire();
+                tsi.setSwitch(3,11,0x02);
+                stn2_under.release();
+              }
+              else{
+                Thread.sleep(1000 + (20 * Math.abs(speed)));
+                tsi.setSpeed(id,0);
+                Thread.sleep(1000 + (20 * Math.abs(speed)));
+                speed = - this.speed;
+                tsi.setSpeed(id,speed);
+              }
+            }
+            //P
+            if(xPos==1 & yPos==11 & stat==0x01){
+              if(dir==true){
+                tsi.setSwitch(3, 11, 0x02);
+                /*if(stn2_over.tryAcquire()){
+                  tsi.setSwitch(3, 11, 0x02);
+                  curve_left.release();
+
+                }
+                else{
+                  stn2_under.acquire();
+                  tsi.setSwitch(3,11,0x01);
+                  curve_left.release(); 
+                
+                }*/
+              }
+              
+            }
+
+          }
         } catch (Exception e) {
           // TODO: handle exception
         }
-        if(xPos == 5 & yPos == 11 & stat == 0x01){ //s1
-          tsi.setSwitch(3,11,0x01);
-        }
-        else if(xPos == 15 & yPos == 7 & stat == 0x01){ //sw1,s1
-          tsi.setSwitch(17,7,0x02);
-        }
-        else if(xPos == 2 & yPos == 9 & stat == 0x01){ //s2
-          tsi.setSwitch(4,9,0x01);
-        }
-        else if(xPos == 13 & yPos == 9 & stat == 0x01){
-          tsi.setSwitch(15,9,0x02);
-        }
+
+        
 
         } 
       catch (Exception e) {
@@ -79,66 +137,4 @@ public class Lab1 {
   }
 }
 
-//tracks
-
-
-
-
-/*public boolean getDir(trainId){
-    if (speed1 < 0);
-      return 0;
-    
-    elseif(speed1 > 0);
-      return 1;
-  
-}
-*/
-//Semaphores
-/* 
-Semaphore track1 = new Semaphore(1);
-Semaphore track2 = new Semaphore(1);
-Semaphore track3 = new Semaphore(1);
-Semaphore track4 = new Semaphore(1);
-Semaphore track5 = new Semaphore(1);
-Semaphore track6 = new Semaphore(1);
-Semaphore track7 = new Semaphore(1);
-Semaphore track8 = new Semaphore(1);
-Semaphore track9 = new Semaphore(1);
-Semaphore track10 = new Semaphore(1);
-
-class shared{
-  static int counter = 0;
-}
-boolean train1dir = 1
-if (speed1 < 0){
-  train1dir = 0
-}elseif(speed1 > 0){
-  traind1dir = 1
-}
-boolean train2dir = 1
-if (speed2 < 0){
-  train2dir = 1
-}elseif(speed2 > 0){
-  traind2dir = 0
-}
-//traindir = 1 är nedåt, traindir = 0 är uppåt oavsett trainId
-//Threads för tågen
-class Train extends Thread {
-  public void main(String[] args){
-    
-    getSensor(1){
-      if(home1 == active && train1dir){
-        tsi.setSwitch(xpos,ypos,0x02);
-      }
-      if(home1 == active && !train1dir){
-        Thread.sleep(1000 + (20 * |train_speed|))
-      }
-    }
-    getSensor(2){
-      if()
-    }
-
-  }
-}
- */
 
