2581375b54a9ece6c0a75d88d0503d1bc2545358
Noah <noahsu@chalmers.se>
2023-09-13 21:24:45 +0200

    Snart där

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -21,17 +21,6 @@ public class Lab1 {
     } catch (Exception e) {
       // TODO: handle exception
     }
-
-    t1.start();
-    t2.start();
-  }
-
-  class Train implements Runnable {
-    TSimInterface tsi;
-    int speed;
-    int id; // 1 för övre tåget, 2 för det undre
-    boolean dir; // TRUE om tåg kör nedåt, FALSE om de kör uppåt
-    
     Semaphore crossing = new Semaphore(1);
     Semaphore stn2_over = new Semaphore(1);
     Semaphore stn2_under = new Semaphore(1);
@@ -41,6 +30,21 @@ public class Lab1 {
     Semaphore straight1_under = new Semaphore(1);
     Semaphore straight2_over = new Semaphore(1);
     Semaphore straight2_under = new Semaphore(1);
+
+    t1.start();
+    t2.start();
+      
+  class Train implements Runnable {
+    TSimInterface tsi;
+    int speed;
+    int id; // 1 för övre tåget, 2 för det undre
+    boolean dir; // TRUE om tåg kör nedåt, FALSE om de kör uppåt
+    //SWITCHAR: true om switchDir==right, false om switchDir==left
+    boolean sw1; //switch längst upp till höger
+    boolean sw2; //switch mitten höger
+    boolean sw3; //switch mitten vänster
+    boolean sw4; //switch längst ner vänster
+    
 
     Train(TSimInterface tsi, int speed, boolean dir, int id) {
       this.tsi = tsi;
@@ -52,13 +56,15 @@ public class Lab1 {
     public void run(){      
           
       try {
-      tsi.setSpeed(id,speed);
-      if(id==1){
-        straight1_over.acquire();
-      }
-      else{
-        stn2_over.acquire();
-      }
+      tsi.setSpeed(id,speed);      
+      straight1_over.acquire();
+      stn2_over.acquire();
+      sw1 = false;
+      sw2 = false;
+      sw3 = false;
+      sw4 = false;
+      
+      System.out.println(straight1_over.availablePermits());
       
       
         while(true){ //längst upp
@@ -70,170 +76,149 @@ public class Lab1 {
           
           
           //switch 4
-          //Q
-          if(xPos==5 & yPos==11 & stat==0x01){
-            if(dir==false){
-              tsi.setSpeed(id,0);
-              curve_left.acquire();
-              tsi.setSwitch(3,11,0x01);
-              tsi.setSpeed(id, speed);
-              
-            }
-            else{
-              curve_left.release();
-              Thread.sleep(1000 + (20 * Math.abs(speed)));
-              tsi.setSpeed(id,0);
-              Thread.sleep(1000 + (20 * Math.abs(speed)));
-              speed = - this.speed;
-              dir = !this.dir;
-              tsi.setSpeed(id,speed);
-            }
-          }
-
-          //R
-          if(xPos==3 & yPos==13 & stat==0x01){
-            if(dir==false){
-              tsi.setSpeed(id,0);
-              curve_left.acquire();
-              tsi.setSwitch(3,11,0x02);
-              tsi.setSpeed(id,speed);
-              
-            }
-            else{
-              curve_left.release();
-              Thread.sleep(1000 + (20 * Math.abs(speed)));
-              tsi.setSpeed(id,0);
-              Thread.sleep(1000 + (20 * Math.abs(speed)));
-              speed = - this.speed;
-              dir = !this.dir;
-              tsi.setSpeed(id,speed);
-            }
-          }
-
-          //P
-          if(xPos==1 & yPos==11 & stat==0x01){
-            if(dir==true){              
-              if(stn2_over.tryAcquire()){
-                tsi.setSwitch(3, 11, 0x01);
-                
-              }
-                else{
-                stn2_under.acquire();
-                tsi.setSwitch(3,11,0x02);
-                 
-              }
-            }
-            else{
-              if(stn2_over.availablePermits()==0){
-                stn2_over.release();                
-              }
-              else{
-                stn2_under.release();
-              }
-            } 
-          }
-
-          //O
-          if(xPos==2 & yPos==9 & stat==0x01){
-            if(dir==false){
-              if(straight2_over.tryAcquire()){
-                tsi.setSwitch(4, 9, 0x01);                
-              }
-              else{
-                straight2_under.acquire();
-                tsi.setSwitch(4, 9, 0x02);                
-              }
-            }
-            else if(dir==true){
-              if(straight2_over.availablePermits()==0){
-                straight2_over.release();
-              }
-              else{
-                straight2_under.release();
-              }
-              
-            }
-          }
-
-          //J
-          if(xPos==17 & yPos==9 & stat==0x01){
+          //B
+          if(xPos==6 && yPos==5 && stat==0x01){
             if(dir==true){
-              if(straight2_over.tryAcquire()){
-                tsi.setSwitch(15, 9, 0x02);
-                
-              }
-              else{
-                straight2_under.acquire();
-                tsi.setSwitch(15, 9, 0x01);                
-              }
+              tsi.setSpeed(id, 0);
+              crossing.acquire();
+              tsi.setSpeed(id, speed);
             }
             else{
-              if(straight2_over.availablePermits()==0){
-                straight2_over.release();
-              }
-              else{
-                straight2_under.release();
-              }
+              crossing.release();
+              Thread.sleep(3000 + (20 * Math.abs(speed)));
+              tsi.setSpeed(id,0);
+              Thread.sleep(1000 + (20 * Math.abs(speed)));
+              speed = - this.speed;
+              dir = !this.dir;
+              tsi.setSpeed(id,speed);
+            }
+          }
+
+          //C
+          if(xPos==10 && yPos==5 && stat==0x01){
+            if(dir==true){
+              tsi.setSpeed(id, 0);
+              crossing.acquire();
+              tsi.setSpeed(id, speed);
+            }
+            else{
+              crossing.release();
+              Thread.sleep(1200 + (20 * Math.abs(speed)));
+              tsi.setSpeed(id,0);
+              Thread.sleep(1000 + (20 * Math.abs(speed)));
+              speed = - this.speed;
+              dir = !this.dir;
+              tsi.setSpeed(id,speed);
             }
           }
           
+          //D
+          if(xPos==11 && yPos==7 && stat==0x01){
+            if(dir==false){
+              tsi.setSpeed(id, 0);
+              crossing.acquire();
+              tsi.setSpeed(id, speed);
+            }
+            else{
+              crossing.release();
+            }
+          }
+
+          //E
+          if(xPos==11 && yPos==8 && stat==0x01){
+            if(dir==false){
+              tsi.setSpeed(id, 0);
+              crossing.acquire();
+              tsi.setSpeed(id, speed);
+            }
+            else{
+              crossing.release();
+            }
+
+          }
+
+          //G
+          if(xPos==14 && yPos==7 && stat==0x01){
+            if(dir==true){  
+              tsi.setSpeed(id, 0);            
+              curve_right.acquire();
+              tsi.setSwitch(17,7,0x02);
+              sw1 = true;
+              tsi.setSpeed(id, speed);                                     
+            }
+            else{
+              curve_right.release();
+            }
+          }
+
           //H
-          if(xPos==19 & yPos==8 & stat==0x01){
+          if(xPos==19 && yPos==8 && stat==0x01){
             if(dir==false){
               if(straight1_over.tryAcquire()){
                 tsi.setSwitch(17, 7, 0x02);
-                
+                sw1 = true;
               }
               else{
                 straight1_under.acquire();
                 tsi.setSwitch(17, 7, 0x01);
-                
+                sw1 = false;                
               }
             }
             else{
-              if(straight1_over.availablePermits()==0){
+              if(sw1){
                 straight1_over.release();
               }
               else{
                 straight1_under.release();
               }
-
             }
           }
 
-          //M
-          if(xPos==7 & yPos==9 & stat==0x01){
-            if(dir==true){
-              tsi.setSpeed(id,0); 
-              curve_left.acquire();
-              tsi.setSwitch(4,9,0x01);
-              tsi.setSpeed(id, speed);
-                              
-            }
-            else{
-              curve_left.release();
-            }
-          }
-
-          //N
-          if(xPos==7 & yPos==10 & stat==0x01){
+          //I
+          if(xPos==16 && yPos==8 && stat==0x01){
             if(dir==true){
               tsi.setSpeed(id,0);
-              curve_left.acquire();
-              tsi.setSwitch(4,9,0x02);                tsi.setSpeed(id, speed);
-              straight2_under.release();                
+              curve_right.acquire();
+              tsi.setSwitch(17,7,0x01);
+              sw1 = false;
+              tsi.setSpeed(id, speed);                              
             }
             else{
-              curve_left.release();
+              curve_right.release();
+            }
+          }
+
+          //J
+          if(xPos==17 && yPos==9 && stat==0x01){
+            if(dir==true){
+              System.out.println("ANTAL permissioner" + straight2_over.availablePermits());
+              if(straight2_over.tryAcquire()){
+                tsi.setSwitch(15, 9, 0x02);
+                sw2 = true;                
+              }
+              else{
+                straight2_under.acquire();
+                tsi.setSwitch(15, 9, 0x01);
+                sw2 = false;                
+              }
+            }
+            else{
+              if(sw2){
+                straight2_over.release();
+              }
+              else{
+                straight2_under.release();
+              }
             }
           }
 
           //K
-          if(xPos==13 & yPos==9 & stat==0x01){
+          if(xPos==12 && yPos==9 && stat==0x01){
             if(dir==false){
               tsi.setSpeed(id,0);
               curve_right.acquire();
               tsi.setSwitch(15,9,0x02);
+              sw2 = true; 
               tsi.setSpeed(id, speed);
                               
             }
@@ -243,25 +228,12 @@ public class Lab1 {
           }
 
           //L
-          if(xPos==13 & yPos==10 & stat==0x01){
+          if(xPos==12 && yPos==10 && stat==0x01){
             if(dir==false){
               tsi.setSpeed(id,0);
               curve_right.acquire();
               tsi.setSwitch(15,9,0x01);
-              tsi.setSpeed(id, speed);
-                             
-            }
-            else{
-              curve_right.release();
-            }
-          }
-
-          //G
-          if(xPos==15 & yPos==7 & stat==0x01){
-            if(dir==true){  
-              tsi.setSpeed(id, 0);            
-              curve_right.acquire();
-              tsi.setSwitch(17,7,0x02);
+              sw2 = false; 
               tsi.setSpeed(id, speed);                             
             }
             else{
@@ -269,29 +241,95 @@ public class Lab1 {
             }
           }
 
-          //I
-          if(xPos==16 & yPos==8 & stat==0x01){
+          //M
+          if(xPos==7 && yPos==9 && stat==0x01){
             if(dir==true){
-              tsi.setSpeed(id,0);
-              curve_right.acquire();
-              tsi.setSwitch(17,7,0x01);
-              tsi.setSpeed(id, speed);                              
+              tsi.setSpeed(id,0); 
+              curve_left.acquire();
+              tsi.setSwitch(4,9,0x01);
+              sw3 = false; 
+              tsi.setSpeed(id, speed);
+                              
             }
             else{
-              curve_right.release();
+              curve_left.release();
             }
           }
 
-          //B
-          if(xPos==6 & yPos==6 & stat==0x01){
+          //N
+          if(xPos==7 && yPos==10 && stat==0x01){
             if(dir==true){
-              tsi.setSpeed(id, 0);
-              crossing.acquire();
+              tsi.setSpeed(id,0);
+              curve_left.acquire();
+              tsi.setSwitch(4,9,0x02);
+              sw3 = true;
               tsi.setSpeed(id, speed);
+              straight2_under.release();                
             }
             else{
-              crossing.release();
-              Thread.sleep(1000 + (20 * Math.abs(speed)));
+              curve_left.release();
+            }
+          }
+
+          //O
+          if(xPos==2 && yPos==9 && stat==0x01){
+            if(dir==false){
+              if(straight2_over.tryAcquire()){
+                System.out.println("ANTAL permissioner " + straight2_over.availablePermits());
+                tsi.setSwitch(4, 9, 0x01);
+                sw3 = false;            
+              }
+              else{
+                straight2_under.acquire();
+                tsi.setSwitch(4, 9, 0x02);
+                sw3 = true;              
+              }
+            }
+            else if(dir==true){
+              if(!sw3){
+                straight2_over.release();
+              }
+              else{
+                straight2_under.release();
+              }
+              
+            }
+          }
+          //P
+          if(xPos==1 && yPos==11 && stat==0x01){
+            if(dir==true){              
+              if(stn2_over.tryAcquire()){
+                tsi.setSwitch(3, 11, 0x01);
+                sw4 = false;              
+              }
+                else{
+                stn2_under.acquire();
+                tsi.setSwitch(3,11,0x02);
+                sw4 = true;                 
+              }
+            }
+            else{
+              if(!sw4){
+                stn2_over.release();                
+              }
+              else{
+                stn2_under.release();
+              }
+            } 
+          }
+          //Q
+          if(xPos==6 && yPos==11 && stat==0x01){
+            if(dir==false){
+              tsi.setSpeed(id,0);
+              curve_left.acquire();
+              tsi.setSwitch(3,11,0x01);
+              sw4 = false;
+              tsi.setSpeed(id, speed);
+              
+            }
+            else{
+              curve_left.release();
+              Thread.sleep(2000 + (20 * Math.abs(speed)));
               tsi.setSpeed(id,0);
               Thread.sleep(1000 + (20 * Math.abs(speed)));
               speed = - this.speed;
@@ -300,53 +338,31 @@ public class Lab1 {
             }
           }
 
-          //D
-          if(xPos==10 & yPos==7 & stat==0x01){
+          //R
+          if(xPos==4 && yPos==13 && stat==0x01){
             if(dir==false){
-              tsi.setSpeed(id, 0);
-              crossing.acquire();
-              tsi.setSpeed(id, speed);
+              tsi.setSpeed(id,0);
+              curve_left.acquire();
+              tsi.setSwitch(3,11,0x02);
+              sw4 = true;
+              tsi.setSpeed(id,speed);
+              
             }
             else{
-              crossing.release();
-            }
-
-          }
-
-          //C
-          if(xPos==9 & yPos==5 & stat==0x01){
-            if(dir==true){
-              tsi.setSpeed(id, 0);
-              crossing.acquire();
-              tsi.setSpeed(id, speed);
-            }
-            else{
-              crossing.release();
-              Thread.sleep(1000 + (20 * Math.abs(speed)));
+              curve_left.release();
+              Thread.sleep(2500 + (20 * Math.abs(speed)));
               tsi.setSpeed(id,0);
               Thread.sleep(1000 + (20 * Math.abs(speed)));
               speed = - this.speed;
               dir = !this.dir;
               tsi.setSpeed(id,speed);
             }
-          }
-
-          //E
-          if(xPos==10 & yPos==7 & stat==0x01){
-            if(dir==false){
-              tsi.setSpeed(id, 0);
-              crossing.acquire();
-              tsi.setSpeed(id, speed);
-            }
-            else{
-              crossing.release();
-            }
-
-          }
+          }          
         }
       } catch (Exception e) {
           // TODO: handle exception
       }
     }
+  }
   }
 }
